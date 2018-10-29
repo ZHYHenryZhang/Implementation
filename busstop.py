2b71c067c373521ccf08b4660fb9e19f15158c8f
ZHYHenryZhang <henryzhang19951213@gmail.com>
2018-10-29 01:57:57 -0700

    comments added

--- busstop.py
+++ busstop.py
@@ -1,31 +1,42 @@
 # bus stop name shorten
+busstop1 = [150, 30, 31 ,41, 42, 101, 39 , 40, 201, 202, 200]
+busstop2 = [1, 2, 3, 4, 5, 6, 9, 10, 33, 55, 54, 32]
 
-original = [150, 30, 31 ,41, 42, 101, 39 , 40, 201, 202, 200]
-original.sort()
-print(original)
-end = len(original)
-a = 0
-b = 0
+def shorten(original):
+    original.sort()
+    print(original)
+    end = len(original)
 
-while a < end - 1:
-    b = a + 1
-    old = a
-    while b < end and original[b] == original[old] + 1:
-        old = b
-        b += 1
-    if original[old] == original[a]:
-        print(original[a], end = ", ")
-    elif original[old] - original[a] == 1:
-        print("{}, {}".format(original[a], original[old]), end = ", ")
-    else:
-        print("{}-{}".format(original[a],original[old]),end = ", ")
-    a = b
+    a = 0 # mark the start
+    b = 0 # mark the end
 
-if a == end - 1:
-    print(original[a])
-print(" ")
+    while a < end: # when the start reaches end of list
+        # move the end forward
+        b = a + 1 
+        old = a
+        
+        # loop until not satisfy
+        while b < end and original[b] == original[old] + 1:
+            old = b
+            b += 1
+        
+        # check looping result:
+        if original[old] == original[a]: # not consecutive
+            print(original[a], end = ", ")
+        elif original[old] - original[a] == 1: # only 2 numbers, so does not make it short
+            print("{}, {}".format(original[a], original[old]), end = ", ")
+        else:
+            print("{}-{}".format(original[a],original[old]),end = ", ")
 
+        # let start catch up with end
+        a = b
 
+    print(" ") # format
+
+shorten(busstop1)
+shorten(busstop2)
+
+# original confusing one, not tested, buggy
 """ i += 1
 if original[i] == old+1:
     if begin == None:
